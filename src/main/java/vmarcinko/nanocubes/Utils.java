386094039dc153da89f024c71229f19ca31913dc
Vjeran Marcinko <vmarcinko@gmail.com>
2014-07-02 23:05:27 +0200

    encoding/decoding optimization utils

--- src/main/java/vmarcinko/nanocubes/Utils.java
+++ src/main/java/vmarcinko/nanocubes/Utils.java
@@ -1,6 +1,8 @@
 package vmarcinko.nanocubes;
 
 public class Utils {
+    private final static String binaryMask = "1111111111111111111111111111111";
+    private final static int intMask = Integer.parseInt(binaryMask, 2);
 
     private Utils() {
     }
@@ -15,5 +17,19 @@ public class Utils {
             throw new IllegalArgumentException("Value cannot be larger than " + maxValue + ", but is " + value);
         }
         return (int) (value + Integer.MIN_VALUE);
+    }
+
+    public static long encodeTwoInts(int a, int b) {
+        long encoded = 0;
+        encoded |= a;
+        encoded <<= binaryMask.length();
+        encoded |= b;
+        return encoded;
+    }
+
+    public static int[] decodeTwoInts(long encodedInts) {
+        int b = (int) (encodedInts & intMask);
+        int a = (int) ((encodedInts >> binaryMask.length()) & intMask);
+        return new int[]{a, b};
     }
 }
